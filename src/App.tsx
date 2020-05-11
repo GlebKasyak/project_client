6915479a7d93598aafe62f2eec840c8af25ac19d
Gleb <gkebkasyak@gmail.com>
2020-05-11 10:47:48 +0300

    dev/chat app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, FC } from "react";
+import { connect } from "react-redux";
+import { Switch, Route } from "react-router-dom";
+import { Beforeunload } from 'react-beforeunload';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Home, UsersPage, DialogsPage, ChatPage, ProfilePage, Page404 } from "./pages";
+import { NavBar } from "./components";
+import { LoginForm, RegisterForm } from "./modules";
+
+import socket from "./socketServices";
+import { AppStateType } from "./store/reducers";
+import { UserSelectors } from "./store/selectors";
+import { Auth, DoNotAuth } from "./hoc";
+import { storageKeys } from "./assets/constants/commons";
+import { urls } from "./assets/constants";
+import { getAuthUserData } from "./store/actions/user.action";
+import "./assets/styles/app.scss";
+
+type MapStateToPropsType = {
+    isAuth?: boolean,
+    userId: string
 }
 
-export default App;
+type MapDispatchToPropsType = {
+    getAuthUserData: () => void
+}
+
+type PropType = MapStateToPropsType & MapDispatchToPropsType;
+
+
+let App: FC<PropType> = ({ isAuth, userId, getAuthUserData }) => {
+  useEffect(() => {
+    const authData = localStorage.getItem(storageKeys.isAuth);
+
+    if(!!authData && JSON.parse(authData)) getAuthUserData();
+    if(isAuth) socket.setOnlineStatus({ userId, isOnline: true });
+  }, [isAuth, getAuthUserData, userId]);
+
+  return (
+      <Beforeunload onBeforeunload={ () => userId && socket.setOnlineStatus({ userId, isOnline: false }) } >
+          <div className={"app " + (isAuth ? "app--active" : "app--not-active")} >
+            <NavBar />
+            <Switch>
+              <Route exact path={ urls.home } component={ DoNotAuth(Home) } />
+              <Route exact path={ urls.users } component={ DoNotAuth(UsersPage) } />
+              <Route exact path={ urls.dialogs } component={ DoNotAuth(DialogsPage) } />
+              <Route exact path={ urls.chat } component={ DoNotAuth(ChatPage) } />
+              <Route exact path={ urls.profile } component={ DoNotAuth(ProfilePage) } />
+
+              <Route path={ urls.login } component={ Auth(LoginForm) } />
+              <Route path={ urls.register } component={ Auth(RegisterForm) } />
+
+              <Route path={ [urls.page404, "*"] } component={ Page404 } />
+            </Switch>
+          </div>
+      </Beforeunload>
+  );
+};
+
+const mapStateToProps = (state: AppStateType) => ({
+    isAuth: UserSelectors.getUserIsAuth(state),
+    userId: UserSelectors.getUserId(state)
+});
+
+export default connect<MapStateToPropsType, MapDispatchToPropsType, {}, AppStateType>(
+    mapStateToProps,
+    { getAuthUserData })
+(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { createBrowserHistory } from "history";
+import { Router } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+import apiServices from "./apiServices";
+import { store } from "./store";
+
+const history = createBrowserHistory();
+apiServices(store, history);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
+    <Provider store={ store }>
+        <Router history={ history } >
+            <App/>
+        </Router>
+    </Provider>,
+    document.getElementById("root"));
