3f2d86656b1180359e2ef36b47a289bd39d4dc96
Gleb <gkebkasyak@gmail.com>
2020-05-13 13:22:51 +0300

    add/add MessageImage component

---
+++ src/components/molecules/Messages/Message/MessageImage/MessageImage.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react"
+
+import { ENV } from "../../../../../assets/constants";
+import "./style.scss";
+
+type Props = {
+  message: string,
+  type: string
+};
+
+const MessageImage: FC<Props> =  ({ message, type }) => (
+    <a
+        href={`${ ENV.SERVER_URL }/${ message }`}
+        className="message__link-msg"
+        target="_blank"
+        rel="noopener noreferrer"
+    >
+      <img
+          src={`${ ENV.SERVER_URL }/${ message }`}
+          className="message__img"
+          alt={ `${ type } message` }
+      />
+    </a>
+)
+
+export default MessageImage;
