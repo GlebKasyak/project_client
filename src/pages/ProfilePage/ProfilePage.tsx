a75b296862229be2f5fd67d96b99531ca728f295
Gleb <gkebkasyak@gmail.com>
2020-05-13 13:27:44 +0300

    refactor/refactor Profile page, add constants

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,55 +1,65 @@
 import React, { FC } from "react";
 import { Divider, Col, Row, Typography, Button } from "antd";
 
-import { DescriptionItem, UploadButton } from "../../components";
+import { DescriptionItem, UploadButton, ErrorMessage } from "../../components";
 
+import { Handlers } from "../../interfaces/common";
 import { IUser } from "../../interfaces/user";
-import { timeFromNow } from "../../shared/helpres";
 import { ENV } from "../../assets/constants";
 import showConfirm from "../../shared/showConfirm";
+import { timeFromNow } from "../../shared/helpres";
 import "./style.scss";
 
 type PropsType = {
     user: IUser,
-    removeUser: () => void
+    removeUser: () => void,
+    onUploadAvatar: Handlers.ChangeType,
+    err: string
 }
 
-const ProfilePage: FC<PropsType> = ({ user, removeUser }) => (
-    <div className="profile container">
-        <Typography.Title level={4} >User Profile</Typography.Title>
-        <p className="profile__section" >Personal</p>
+const ProfilePage: FC<PropsType> = ({ user, removeUser, onUploadAvatar, err }) => (
+    <>
+        { !!err && <ErrorMessage text={ err } /> }
+        <div className="profile container">
+            <Typography.Title level={4} >User Profile</Typography.Title>
+            <p className="profile__section" >Personal</p>
 
-        <DescriptionItem title="The first name" content={ user.firstName } />
-        <DescriptionItem title="The second name" content={ user.secondName } />
-        <DescriptionItem title="Register date" content={ timeFromNow(user.createdAt!) } />
+            <DescriptionItem title="The first name" content={ user.firstName } />
+            <DescriptionItem title="The second name" content={ user.secondName } />
+            <DescriptionItem title="Register date" content={ timeFromNow(user.createdAt!) } />
 
-        <Row>
-            <Col md={12} xs={24} className="profile__avatar" >
-                <DescriptionItem title="Avatar" />
-                <div className="profile-avatar-wrapper" >
-                    <img
-                        src={ user.avatar && `${ ENV.SERVER_URL }/${ user.avatar }` }
-                        alt={ user.firstName }
-                        className="profile-avatar img"
+            <Row>
+                <Col md={12} xs={24} className="profile__avatar" >
+                    <DescriptionItem title="Avatar" />
+                    <div className="profile-avatar-wrapper" >
+                        <img
+                            src={ user.avatar && `${ ENV.SERVER_URL }/${ user.avatar }` }
+                            alt={ user.firstName }
+                            className="profile-avatar img"
+                        />
+                    </div>
+                    <UploadButton
+                        text="Update avatar"
+                        iconType="upload"
+                        callback={ onUploadAvatar }
                     />
-                </div>
-                <UploadButton text="Update avatar" />
-            </Col>
-        </Row>
+                </Col>
+            </Row>
 
-        <Divider />
-        <p className="profile__section" >Contacts</p>
+            <Divider />
+            <p className="profile__section" >Contacts</p>
 
-        <DescriptionItem title="Email" content={ user.email } />
-        <Button
-            onClick={ () => showConfirm(removeUser, "Do you Want to delete this account?") }
-            type="danger"
-            icon="delete"
-            className="w-100 btn"
-        >
-            Remove account
-        </Button>
-    </div>
+            <DescriptionItem title="Email" content={ user.email } />
+            <Button
+                onClick={ () => showConfirm(removeUser, "Do you Want to delete this account?") }
+                type="danger"
+                icon="delete"
+                className="w-100 btn"
+            >
+                Remove account
+            </Button>
+        </div>
+    </>
 )
 
 export default ProfilePage;
--- src/pages/ProfilePage/ProfilePageContainer.tsx
+++ src/pages/ProfilePage/ProfilePageContainer.tsx
@@ -1,11 +1,14 @@
+import React, { FC, useState } from "react";
 import { connect } from "react-redux";
 
 import ProfilePage from "./ProfilePage";
 
+import { TypesFileEnum } from "../../assets/constants/api.contsnts";
+import { Handlers } from "../../interfaces/common";
 import { UserSelectors } from "../../store/selectors";
 import { AppStateType } from "../../store/reducers";
 import { IUser } from "../../interfaces/user";
-import { removeUser } from "../../store/actions/user.action";
+import { removeUser, ThunkDispatchUsersType, uploadAvatar } from "../../store/actions/user.action";
 
 
 type MapStateToProps = {
@@ -13,10 +16,36 @@ type MapStateToProps = {
 }
 
 type MapDispatchToProps = {
-  removeUser: () => void
+  removeUser: () => void,
+  uploadAvatar: (type: TypesFileEnum.avatar, file: File) => Promise<boolean>
 }
+
+type Props = MapStateToProps & MapDispatchToProps;
+
+const ProfilePageContainer: FC<Props> = ({ user, removeUser, uploadAvatar }) => {
+  const [err, setErr] = useState("");
+
+  const handleUploadAvatar: Handlers.ChangeType = async e => {
+    setErr("");
+
+    const response = await uploadAvatar(TypesFileEnum.avatar, e.target.files![0]);
+    !response && setErr("Error! Incorrect type of file!");
+  }
+
+  return <ProfilePage
+      user={ user }
+      removeUser={ removeUser }
+      onUploadAvatar={ handleUploadAvatar }
+      err={ err }
+  />
+};
+
+const mapDispatchToProps = (dispatch: ThunkDispatchUsersType) => ({
+  removeUser: () => dispatch(removeUser()),
+  uploadAvatar: (type: TypesFileEnum.avatar, file: File) => dispatch(uploadAvatar(type, file))
+});
 
 export default connect<MapStateToProps, MapDispatchToProps, {}, AppStateType>(
     state => ({  user: UserSelectors.getUser(state) }),
-    { removeUser }
-)(ProfilePage);
+    mapDispatchToProps
+)(ProfilePageContainer);
