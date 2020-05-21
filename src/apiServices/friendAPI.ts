a15439ffb67471557ba095cefbf1ad5b3ebe45bd
Gleb <gkebkasyak@gmail.com>
2020-05-22 01:41:56 +0300

    create/create friendsAPI

--- src/apiServices/friendAPI.ts
+++ src/apiServices/friendAPI.ts
@@ -1,6 +1,6 @@
-import axios, { CancelToken } from "axios";
+import axios from "axios";
 
-import { MainEndpoints } from "../assets/constants/api.contsnts";
+import { MainEndpoints } from "../shared/constants/api.contsnts";
 import { ScrollDataType } from "../interfaces/common";
 
 class FriendAPI {
@@ -10,8 +10,8 @@ class FriendAPI {
     static removeFriend = (userId: string) =>
         axios.get(`${ MainEndpoints.friend }/remove-friend/${ userId }`);
 
-    static getFriendsById = (data: ScrollDataType, token?: CancelToken) =>
-        axios.get(`${ MainEndpoints.friend }/friends/${ JSON.stringify(data) }`, { cancelToken: token });
+    static getFriendsById = (data: ScrollDataType) =>
+        axios.get(`${ MainEndpoints.friend }/friends/${ JSON.stringify(data) }`);
 
     static searchFriends = (userId: string, value: string) =>
         axios.post(`${ MainEndpoints.friend }/search`, { userId, value });
